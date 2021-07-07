a1e1f764b1ae17ccb48fb11a77304e7eca9c9f64
mdquynhhuong <mdquynhhuong@gmail.com>
2021-07-07 10:21:37 -0400

    adding javascript

---
+++ javascripts.js
@@ -0,0 +1,23 @@
+var menu = document.querySelector(".menu")
+var hamburgermenu = document.querySelector(".hamburgermenu")
+var menuIcon = document.querySelector(".menuIcon")
+
+hamburgermenu.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    menuIcon.style.display = "none";
+  }
+}
+
+var menuLinks = document.querySelectorAll(".menuLink")
+
+menuLinks.forEach(
+  function (menuLink) {
+    menuLink.addEventListener("click", toggleMenu)
+  }
+)
